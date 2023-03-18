89ec32229f9e2e4101d3ecdb77c47ed13b9b1507
Alex Geis <siegxela@gmail.com>
2023-03-17 21:45:53 -0600

    update /Carousel/Controls.tsx components Controls and IconButton - add props, html display using props

--- react-practice/src/components/Carousel/components/Controls.tsx
+++ react-practice/src/components/Carousel/components/Controls.tsx
@@ -1,5 +1,27 @@
-import React from "react";
+import { ReactNode } from "react";
 
-export default function Controls() {
-	return <div>Controls</div>;
+export function Controls({ children }: { children: ReactNode }): JSX.Element {
+	return (
+		<div
+			style={{
+				position: "absolute",
+				bottom: "0",
+				right: "0",
+				display: "flex",
+				gap: "10px",
+			}}
+		>
+			{children}
+		</div>
+	);
+}
+
+export function IconButton({
+	onClick,
+	children,
+}: {
+	children: ReactNode;
+	onClick: React.MouseEventHandler<HTMLButtonElement>;
+}): JSX.Element {
+	return <button onClick={onClick}>{children}</button>;
 }
